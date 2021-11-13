d249ed870e348a0bbe4b3253f2cc253828cecf24
Shirish Goyal <36202011+shirish-goyal@users.noreply.github.com>
2021-11-13 22:52:43 +0530

    Update Play.java
    changed variables and spilt start fucntion

--- src/org/cricket/Play.java
+++ src/org/cricket/Play.java
@@ -4,56 +4,56 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Play {
 
-	private Match mt;
-	private String first;
-	private String second;
-	protected Shot sh = new Shot();
+	private Match objMatch;
+	private String inningFirst;
+	private String innningSecond;
+	protected Shot objShotTim = new Shot();
 
-	public Play(Match match, String first, String second) {
-		this.mt = match;
-		this.first = first;
-		this.second = second;
+	public Play(Match match, String inningFirst, String innningSecond) {
+		this.objMatch = match;
+		this.inningFirst = inningFirst;
+		this.innningSecond = innningSecond;
 	}
 
-	public String start(int wickets, int overs) {
+	public String start(int totalWickets, int totalOvers) {
 		Commentary cs = new Commentary();
-		String batingTeam = first;
-		boolean isSuperOver = mt.isSuperOverPlayed();
+		String batingTeam = inningFirst;
+		boolean isSuperOver = objMatch.isSuperOverPlayed();
 		for (int j = 0; j < 2; j++) {
 			cs.getCommentaryForOpening(batingTeam);
-			int wicket = wickets;
+			int wicketRemain = totalWickets;
 			int score = 0, ballsBowled = 0;
 			boolean exitFlag = false;
-			for (int k = 0; k < overs && wicket > 0; k++) {
+			for (int k = 0; k < totalOvers && wicketRemain > 0; k++) {
 				int i = 0;
-				for (i = 0; i < 6 && wicket > 0; i++) {
+				for (i = 0; i < 6 && wicketRemain > 0; i++) {
 
-					Bowling bb = new Bowling();
-					String ball = bb.GetBowlingCards();
-					Batting bw = new Batting();
-					String bat = bw.GetBattingCards();
-					String str = sh.GetShotTimings();
-					String temp = calScore(str);
-					int scoreOnBall = Integer.parseInt(temp.substring(0, temp.indexOf(',')));
+					Bowling objBowl = new Bowling();
+					String ball = objBowl.GetBowlingCards();
+					Batting objBat = new Batting();
+					String bat = objBat.GetBattingCards();
+					String timing = objShotTim.GetShotTimings();
+					String outComeOnBall = calScore(timing);
+					int scoreOnBall = Integer.parseInt(outComeOnBall.substring(0, outComeOnBall.indexOf(',')));
 					score += scoreOnBall;
-					String wicketOnBall = temp.substring(temp.indexOf(',') + 1);
-					wicket = "true".equalsIgnoreCase(wicketOnBall) ? wicket - 1 : wicket;
+					String wicketOnBall = outComeOnBall.substring(outComeOnBall.indexOf(',') + 1);
+					wicketRemain = "true".equalsIgnoreCase(wicketOnBall) ? wicketRemain - 1 : wicketRemain;
 					cs.getCommentary(ball, bat, scoreOnBall, wicketOnBall);
 
 					if (!isSuperOver) {
-						mt.getTeams().get(batingTeam).setScore(score);
-						mt.getTeams().get(batingTeam).setWicketsLost(wickets - wicket);
-						mt.getTeams().get(batingTeam).setBallsPlayed((k * 6) + i + 1);
+						objMatch.getTeams().get(batingTeam).setScore(score);
+						objMatch.getTeams().get(batingTeam).setWicketsLost(totalWickets - wicketRemain);
+						objMatch.getTeams().get(batingTeam).setBallsPlayed((k * 6) + i + 1);
 					} else {
-						mt.getTeams().get(batingTeam).setSuperOverscore(score);
-						mt.getTeams().get(batingTeam).setSuperOverWicketsLost(wickets - wicket);
-						mt.getTeams().get(batingTeam).setSuperOverBallsPlayed(i + 1);
-						System.out.println("Super Over : " + batingTeam + " " + score + "/" + (wickets - wicket) + " in "
-								+ (i + 1) + " balls.");
+						objMatch.getTeams().get(batingTeam).setSuperOverscore(score);
+						objMatch.getTeams().get(batingTeam).setSuperOverWicketsLost(totalWickets - wicketRemain);
+						objMatch.getTeams().get(batingTeam).setSuperOverBallsPlayed(i + 1);
+						System.out.println("Super Over : " + batingTeam + " " + score + "/" + (totalWickets - wicketRemain)
+								+ " in " + (i + 1) + " balls.");
 					}
-					if (batingTeam.equals(first)
-							|| (batingTeam.equals(second) && ((!isSuperOver && mt.getTeams().get(first).getScore() >= score)
-									|| (isSuperOver && mt.getTeams().get(first).getSuperOverscore() >= score)))) {
+					if (batingTeam.equals(inningFirst) || (batingTeam.equals(innningSecond) && ((!isSuperOver
+							&& objMatch.getTeams().get(inningFirst).getScore() >= score)
+							|| (isSuperOver && objMatch.getTeams().get(inningFirst).getSuperOverscore() >= score)))) {
 						continue;
 					} else {
 						exitFlag = true;
@@ -63,65 +63,53 @@ public class Play {
 				if (exitFlag) {
 					ballsBowled = (k * 6) + i + 1;
 					break;
-				} else if (wicket == 0) {
+				} else if (wicketRemain == 0) {
 					ballsBowled = (k * 6) + i;
 					break;
 				} else {
-					ballsBowled = (k+1) * 6;
-					System.out.println("\n" + batingTeam + " is at " + score + "/" + (wickets - wicket) + " after "
+					ballsBowled = (k + 1) * 6;
+					System.out.println("\n" + batingTeam + " is at " + score + "/" + (totalWickets - wicketRemain) + " after "
 							+ (k + 1) + " over(s).\n");
 				}
 			}
-			String overBowled = (ballsBowled % 6 == 0) ? String.valueOf(ballsBowled / 6)
-					: String.valueOf(ballsBowled / 6) + "." + String.valueOf(ballsBowled % 6);
-			System.out.println();
-			System.out.println(batingTeam + " " + score + "/" + (wickets - wicket) + " " + overBowled + " over(s)");
+			String overBowled = oversBowledInAnInning(ballsBowled);
 
-			batingTeam = second;
-		}
-		String result = "";
-		if (!isSuperOver) {
-			if (mt.getTeams().get(first).getScore() > mt.getTeams().get(second).getScore()) {
-				result = first + " won by "
-						+ (mt.getTeams().get(first).getScore() - mt.getTeams().get(second).getScore()) + " runs";
-				mt.setWinner(first);
-			} else if (mt.getTeams().get(first).getScore() < mt.getTeams().get(second).getScore()) {
-				result = second + " won by " + (wickets - mt.getTeams().get(second).getWicketsLost()) + " wickets";
-				mt.setWinner(second);
-			} else {
-				result = "It's a Draw. Match will now move into SuperOver";
-				mt.setSuperOverPlayed(true);
-			}
-		} else {
-			if (mt.getTeams().get(first).getSuperOverscore() > mt.getTeams().get(second).getSuperOverscore()) {
-				result = first + " won in the super over.";
-				mt.setWinner(first);
-			} else if (mt.getTeams().get(first).getSuperOverscore() < mt.getTeams().get(second).getSuperOverscore()) {
-				result = second + " won in the super over.";
-				mt.setWinner(second);
-			} else {
-				result = "Match is a Draw.";
-			}
-		}
+			System.out.println(
+					"\n" + batingTeam + " " + score + "/" + (totalWickets - wicketRemain) + " " + overBowled + " over(s)");
 
+			batingTeam = innningSecond;
+		}
+		String result = getOutcomeAfterBothInnings(isSuperOver, totalOvers);
 		return result;
 	}
 
-	public String calScore(String str) {
+	public String oversBowledInAnInning(int ballsBowled) {
+		String overBowled;
+		try {
+			overBowled = (ballsBowled % 6 == 0) ? String.valueOf(ballsBowled / 6)
+					: String.valueOf(ballsBowled / 6) + "." + String.valueOf(ballsBowled % 6);
+		} catch (Exception e) {
+			overBowled = "0";
+			e.printStackTrace();
+		}
+		return overBowled;
+	}
+
+	public String calScore(String timing) {
 		int score = 0;
 		boolean wicket = false;
-		if (str.equals("Early")) {
+		if (timing.equals("Early")) {
 			int val = ThreadLocalRandom.current().nextInt(-1, 3);
 			if (val == -1)
 				wicket = true;
 			else
 				score = val;
-		} else if (str.equals("Good")) {
+		} else if (timing.equals("Good")) {
 			int val = ThreadLocalRandom.current().nextInt(2, 3);
 			score = val;
-		} else if (str.equals("Perfect")) {
+		} else if (timing.equals("Perfect")) {
 			score = ThreadLocalRandom.current().nextInt(4, 7);
-		} else if (str.equals("Late")) {
+		} else if (timing.equals("Late")) {
 			int val = ThreadLocalRandom.current().nextInt(-1, 2);
 			if (val == -1)
 				wicket = true;
@@ -129,5 +117,37 @@ public class Play {
 				score = val;
 		}
 		return score + "," + wicket;
+	}
+
+	public String getOutcomeAfterBothInnings(boolean isSuperOver, int totalOvers) {
+		String result = "";
+		if (!isSuperOver) {
+			if (objMatch.getTeams().get(inningFirst).getScore() > objMatch.getTeams().get(innningSecond).getScore()) {
+				result = inningFirst + " won by " + (objMatch.getTeams().get(inningFirst).getScore()
+						- objMatch.getTeams().get(innningSecond).getScore()) + " runs";
+				objMatch.setWinner(inningFirst);
+			} else if (objMatch.getTeams().get(inningFirst).getScore() < objMatch.getTeams().get(innningSecond)
+					.getScore()) {
+				result = innningSecond + " won by "
+						+ (totalOvers - objMatch.getTeams().get(innningSecond).getWicketsLost()) + " wickets";
+				objMatch.setWinner(innningSecond);
+			} else {
+				result = "It's a Draw. Match will now move into SuperOver";
+				objMatch.setSuperOverPlayed(true);
+			}
+		} else {
+			if (objMatch.getTeams().get(inningFirst).getSuperOverscore() > objMatch.getTeams().get(innningSecond)
+					.getSuperOverscore()) {
+				result = inningFirst + " won in the super over.";
+				objMatch.setWinner(inningFirst);
+			} else if (objMatch.getTeams().get(inningFirst).getSuperOverscore() < objMatch.getTeams().get(innningSecond)
+					.getSuperOverscore()) {
+				result = innningSecond + " won in the super over.";
+				objMatch.setWinner(innningSecond);
+			} else {
+				result = "Match is a Draw.";
+			}
+		}
+		return result;
 	}
 }
